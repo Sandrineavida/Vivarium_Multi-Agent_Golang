c9c0392bcb6ea84cc81edf3394e57efb3ab4f577
ZhenyangXU <1643013085@qq.com>
2023-12-16 16:56:51 +0100

    修复了每轮仿真，昆虫都掉能量的bug 昆虫每次吃的时候能量加多一点就行

--- server.go
+++ server.go
@@ -398,12 +398,13 @@ func simulateInsecte(ins *organisme.Insecte, allOrganismes []organisme.Organisme
 
 	// 判断并执行 SeReproduire，更新昆虫的繁殖意愿
 	ins.AvoirEnvieReproduire()
-	fmt.Println("我是 ", ins.Espece, "", ins.GetID(), "", ins.EnvieReproduire)
-	// 执行 SeReproduire 行为
+	//fmt.Println("我是 ", ins.Espece, "", ins.GetID(), "", ins.EnvieReproduire)
+	// 执行 SeReproduire 行为 若果打赢了，就想再找昆虫bang，findTargetAganin为true，但也就找这么一次
 	nbBebes, newOrganismes, findTargetAganin := ins.SeReproduire(allOrganismes, terr)
 	if findTargetAganin {
 		nbBebes, newOrganismes, _ = ins.SeReproduire(allOrganismes, terr)
 	}
+
 	// 如果干出东西了，需要更新到服务器中并显示在terrain上
 	if nbBebes != 0 {
 		ecosystemMutex.Lock()
